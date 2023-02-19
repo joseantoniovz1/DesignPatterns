fb19f637dc5f32da54f288fb9209bd5e85abb64c
joseantoniovz1 <vazquez.joseantonio1@gmail.com>
2023-02-19 12:25:30 -0600

    First commit

---
+++ src/com/designpatterns/memento/EditorState.java
@@ -0,0 +1,12 @@
+package com.designpatterns.memento;
+
+public class EditorState {
+    private String content;
+    public EditorState(String content) {
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
+
+}
