afabba9eb3f26d99cf47adfb21aebc2291ee7c16
joseantoniovz1 <vazquez.joseantonio1@gmail.com>
2023-02-19 14:39:48 -0600

    Strategy pattern

---
+++ src/com/designpatterns/strategy/ImageStore.java
@@ -0,0 +1,18 @@
+package com.designpatterns.strategy;
+
+public class ImageStore {
+
+    private Compressor compressor;
+    private Filter filter;
+
+    public ImageStore(Compressor compressor, Filter filter) {
+        this.compressor = compressor;
+        this.filter = filter;
+    }
+
+    public void store(String fileName) {
+        compressor.compress(fileName);
+        filter.apply(fileName);
+    }
+
+}
